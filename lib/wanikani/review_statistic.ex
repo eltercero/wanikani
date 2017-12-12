1f898febaa48eb238e4480c1b80c2874e2213d6c
Victor <victor.martin84@gmail.com>
2017-12-12 13:46:43 +0100

    Wanikani.ReviewStatistic

---
+++ lib/wanikani/review_statistic.ex
@@ -0,0 +1,34 @@
+defmodule Wanikani.ReviewStatistic do
+  @moduledoc """
+  Module for the requests about ReviewStatistics endpoint
+  """
+
+  @doc """
+  Get a list of review statistics.
+
+  Accepted params:
+  * ids Integers  Return results tied to ids
+  * subject_ids Integers  Return result tied to subject_ids
+  * subject_type  radical, kanji, vocabulary  Return results of specified subject_type
+  * updated_after ISO8601 timestamp in the past Return results which have been updated after the timestamp
+
+  ```
+    Wanikani.ReviewStatistic.list(%{subject_type: "kanji"})
+  ```
+  """
+  def list(params, client \\ Wanikani.Api) do
+    client.request("review_statistics", params)
+  end
+
+  @doc """
+  Get a single review stat.
+
+  ```
+    Wanikani.ReviewStatistic.get(64591295)
+  ```
+  """
+  def get(subject_id, client \\ Wanikani.Api) do
+    client.request("review_statistics/#{subject_id}")
+  end
+
+end
---
+++ test/wanikani/review_statistic_test.exs
@@ -0,0 +1,57 @@
+defmodule Wanikani.ReviewStatisticTest do
+  use ExUnit.Case
+
+  defmodule MockApi do
+    def request(_params, _paths) do
+      {:ok,
+     %{"data" => [%{"data" => %{"created_at" => "2017-04-20T21:31:10.806649Z",
+            "meaning_correct" => 11, "meaning_current_streak" => 11,
+            "meaning_incorrect" => 1, "meaning_max_streak" => 11,
+            "percentage_correct" => 92, "reading_correct" => 11,
+            "reading_current_streak" => 3, "reading_incorrect" => 1,
+            "reading_max_streak" => 8, "subject_id" => 1377,
+            "subject_type" => "kanji"},
+          "data_updated_at" => "2017-11-28T07:55:20.016831Z", "id" => 411994,
+          "object" => "review_statistic",
+          "url" => "https://www.wanikani.com/api/v2/review_statistics/411994"},
+        %{"data" => %{"created_at" => "2017-04-07T21:34:57.751344Z",
+            "meaning_correct" => 14, "meaning_current_streak" => 1,
+            "meaning_incorrect" => 4, "meaning_max_streak" => 6,
+            "percentage_correct" => 85, "reading_correct" => 14,
+            "reading_current_streak" => 14, "reading_incorrect" => 1,
+            "reading_max_streak" => 14, "subject_id" => 1339,
+            "subject_type" => "kanji"},
+          "data_updated_at" => "2017-11-28T07:53:56.674025Z", "id" => 412199,
+          "object" => "review_statistic",
+          "url" => "https://www.wanikani.com/api/v2/review_statistics/412199"}]}}
+    end
+
+    def request(_path) do
+      {:ok,
+       %{"data" => %{"created_at" => "2017-04-07T21:34:57.751344Z",
+           "meaning_correct" => 14, "meaning_current_streak" => 1,
+           "meaning_incorrect" => 4, "meaning_max_streak" => 6,
+           "percentage_correct" => 85, "reading_correct" => 14,
+           "reading_current_streak" => 14, "reading_incorrect" => 1,
+           "reading_max_streak" => 14, "subject_id" => 1339,
+           "subject_type" => "kanji"},
+         "data_updated_at" => "2017-11-28T07:53:56.674025Z", "id" => 412199,
+         "object" => "review_statistic",
+         "url" => "https://www.wanikani.com/api/v2/review_statistics/412199"}}
+    end
+  end
+
+  test ".list" do
+    params = %{type: "radical", levels: "1,2"}
+    {status, body} = Wanikani.ReviewStatistic.list(params, MockApi)
+    assert :ok == status
+    assert 2 == Enum.count(body["data"])
+  end
+
+  test ".get" do
+    {status, body} = Wanikani.ReviewStatistic.get(493, MockApi)
+    assert :ok == status
+    assert "https://www.wanikani.com/api/v2/review_statistics/412199" == body["url"]
+  end
+
+end
