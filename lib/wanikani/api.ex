f598f07ecc2afae8401c14598420887b767ce94b
Victor <victor.martin84@gmail.com>
2017-12-12 12:21:50 +0100

    Wanikani.Api

---
+++ lib/wanikani/api.ex
@@ -0,0 +1,23 @@
+defmodule Wanikani.Api do
+  @url "https://www.wanikani.com/api/v2"
+
+  def request(path, params \\ []) do
+    encoded_params = URI.encode_query(params)
+
+    case HTTPoison.get("#{@url}/#{path}?#{encoded_params}", request_headers()) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        {:ok, Poison.decode!(body) }
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        {:error, %{"message": reason} }
+    end
+  end
+
+  defp request_headers do
+    %{"Authorization" => "Token token=#{api_token()}"}
+  end
+
+  defp api_token do
+    Application.get_env(:wanikani, :api_token, System.get_env("WANIKANI_API_TOKEN"))
+  end
+
+end
