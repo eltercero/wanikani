dc8f9c09284341500a19e687fd9371091660822e
Victor <victor.martin84@gmail.com>
2017-12-12 12:58:26 +0100

    User docs

--- lib/wanikani/user.ex
+++ lib/wanikani/user.ex
@@ -1,5 +1,15 @@
 defmodule Wanikani.User do
+  @moduledoc """
+  Module for the requests about User endpoint
+  """
 
+  @doc """
+  Get the user information.
+
+  ```
+   Wanikani.User.get
+  ```
+  """
   def get(client \\ Wanikani.Api) do
     client.request("user")
   end
